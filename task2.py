f69f607767e0367e9f07ba078f825ad73063e4a1
arshadnit123 <arshad.nitt@gmail.com>
2024-06-19 14:29:50 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -43,19 +43,18 @@ df_e=df[selected_columns]
 
 #*****************splitting the data*********************
 
-# Split data into training and validation sets
-X = df_e.drop('seller_price', axis=1)
-y = df_e['seller_price']
+X=df_e.drop('seller_price', axis=1)
+y=df_e['seller_price']
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, shuffle=False)
 
 null_indices_train=X_train[X_train.isnull().any(axis=1)].index
 null_indices_valid=X_valid[X_valid.isnull().any(axis=1)].index
 
-X_train = X_train.drop(index=null_indices_train)
-y_train = y_train.drop(index=null_indices_train)
+X_train=X_train.drop(index=null_indices_train)
+y_train=y_train.drop(index=null_indices_train)
 
-X_valid = X_valid.drop(index=null_indices_valid)
-y_valid = y_valid.drop(index=null_indices_valid)
+X_valid=X_valid.drop(index=null_indices_valid)
+y_valid=y_valid.drop(index=null_indices_valid)
 
 base_models =[
     ("Linear Regression", LinearRegression()),
@@ -63,10 +62,10 @@ base_models =[
     ('Random Forest', RandomForestRegressor(n_estimators=100, random_state=42))
 ]
 
-kf=KFold(n_splits=5, shuffle=True, random_state=42)
+kf=KFold(n_splits=5,shuffle=True,random_state=42)
 
 
-voting_regressor=StackingRegressor(estimators=base_models, final_estimator=LinearRegression())
+voting_regressor=StackingRegressor(estimators=base_models,final_estimator=LinearRegression())
 
 voting_regressor.fit(X_train, y_train)
 
@@ -78,20 +77,18 @@ print(f"Voting Model MAE: {mae:.2f}")
 r2 = r2_score(y_valid, predictions)
 print(f"R-squared (R2) score: {r2:.4f}")
 
-
 dump(voting_regressor, 'voting_regressor_model.joblib')
 dump(encoding_mappings, 'encoding_mappings.joblib')
 
 
 #************Test with values*****************
 
+encoding_mappings=load('encoding_mappings.joblib')
+voting_regressor= load('voting_regressor_model.joblib')
 
-encoding_mappings = load('encoding_mappings.joblib')
-voting_regressor = load('voting_regressor_model.joblib')
+cat_columns=['seller_country', 'seller_badge','product_category', 'clothing_type','region','product_color','product_condition','product_material']
 
-cat_columns = ['seller_country', 'seller_badge','product_category', 'clothing_type','region','product_color','product_condition','product_material']
-
-input_data = {
+input_data= {
     'seller_country': ['Italy'],
     'seller_badge': ['Trusted'],
     'product_category': ['Men Clothing'],
@@ -102,14 +99,14 @@ input_data = {
     'product_material': ['Cotton']
 }
 
-input_df = pd.DataFrame(input_data)
+input_df=pd.DataFrame(input_data)
 
 for col in cat_columns:
-    input_df[col + '_e'] = input_df[col].map(encoding_mappings[col])
+    input_df[col+'_e'] =input_df[col].map(encoding_mappings[col])
 
 selected_columns = [col + '_e' for col in cat_columns]
-input_df = input_df[selected_columns]
+input_df=input_df[selected_columns]
 
-predictions = round(voting_regressor.predict(input_df)[0],2)
+predictions=round(voting_regressor.predict(input_df)[0],2)
 print(predictions)
 
