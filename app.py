24d51a94ef4bc2aea82645fbf0349815d30d0fd9
arshadnit <129346119+arshadnit@users.noreply.github.com>
2024-06-19 13:23:38 +0400

    Add voting_regressor_model.joblib to task2sigma folder

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f43fab996ca254a01363caeb75da7accf630cf2690be9fe5f73e15b9bffc8e9
+size 9097
---
+++ task2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e3c59abc7676b81c447e16f0cb8e6e9519060371bd77dc39ba2a4c992dd23376
+size 3605
